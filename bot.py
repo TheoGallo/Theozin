0038897838a5b1b46984be22d2eed5167e0bcd52
TheoGallo <estelacongentino@gmail.com>
2026-04-27 19:27:23 -0300

    uplod do bot
    Refactor bot to use commands extension and simplify event handling.

--- bot.py
+++ bot.py
@@ -1,33 +1,24 @@
 import discord
-import random
+from discord.ext import commands
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
-
-# A variável intents armazena as permissões do bot
 intents = discord.Intents.default()
-# Ativar a permissão para ler o conteúdo das mensagens
 intents.message_content = True
-# Criar um bot e passar as permissões
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
 async def on_ready():
-    print(f'Fizemos login como {client.user}')
+    print(f'Estamos logados como {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hello!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    else:
-        await message.channel.send(message.content)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("hello, i am bot")
 
-client.run("COLOQUE SEU TOKEN")
+@bot.command()
+async def bye(ctx):
+    await ctx.send("bye, always return")
+@bot.command()
+
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send(f'{member} joined on {member.joined_at}')
+
+bot.run("COLOQUE SEU TOKEN")
